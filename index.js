8aec0008885cae8dd03571037a51d8f70f77d828
Samantha Kane <s.kane133@gmail.com>
2019-08-04 18:04:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Code your solutions in this file
+const name = ["Ada", "Brendan", "Ali"];
+const event = "birthday"
+
 writeCards(name, event) {
   for (let 1 = 0; i <  name.length  ; i++){
     console.log(`Thank you, ${name}, for the wonderful ${event} gift.`);
