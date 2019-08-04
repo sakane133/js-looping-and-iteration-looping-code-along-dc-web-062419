1fc9c216dae87cbb681d33dace02fa538c20c6a0
Samantha Kane <s.kane133@gmail.com>
2019-08-04 18:06:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 // Code your solutions in this file
 
 function writeCards(name, event) {
+   let cards = []
   for (let 1 = 0; i <  name.length  ; i++){
-    console.log(`Thank you, ${name[i]}, for the wonderful ${event} gift.`);
+     cards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift.`);
   }
+  return cards;
 }
 
-// function writeCards( namesArray, event ) {
-//   let thankYouCards = []
-//   for ( let i = 0; i < namesArray.length; i++ ) {
-//     thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
-//   }
-//   return thankYouCards
-// }
+
