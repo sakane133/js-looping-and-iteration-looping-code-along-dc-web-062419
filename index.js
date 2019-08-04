f0279787be80d9d9a827003e493b71328620d225
Samantha Kane <s.kane133@gmail.com>
2019-08-04 18:12:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(name, event) {
+  let cards = []
+  for (let i = 0; i <  name.length  ; i++){
+    cards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+  }
+  return cards;
+}
+
+  function countdown(count){
+    while(count >0){
+      console.log (count);
+      count -= 1;
+    }
+    console.log(count)
+  }
+
+
+
+
