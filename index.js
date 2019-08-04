32e0de6b4538b7152fa20af1c1d03c0d300398d3
Samantha Kane <s.kane133@gmail.com>
2019-08-04 18:06:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ function writeCards(name, event) {
   for (let 1 = 0; i <  name.length  ; i++){
     console.log(`Thank you, ${name[i]}, for the wonderful ${event} gift.`);
   }
-
 }
 
-function writeCards( namesArray, event ) {
-  let thankYouCards = []
-  for ( let i = 0; i < namesArray.length; i++ ) {
-    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
-  }
-  return thankYouCards
-}
+// function writeCards( namesArray, event ) {
+//   let thankYouCards = []
+//   for ( let i = 0; i < namesArray.length; i++ ) {
+//     thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+//   }
+//   return thankYouCards
+// }
